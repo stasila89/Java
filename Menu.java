838bcbdf5e54477568a2b0dec5d3e8df3c02fc25
Stas Piskovski <stasila89@gmail.com>
2023-07-12 18:28:53 +0300

    Update Menu

--- Menu.java
+++ Menu.java
@@ -1,6 +1,18 @@
 package ru.Piskovski;
 
 import ru.Piskovski.HW1.Exercise1;
+import ru.Piskovski.HW1.Exercise2;
+import ru.Piskovski.HW1.Exercise3;
+import ru.Piskovski.HW2.Exercise4;
+import ru.Piskovski.HW2.Exercise5;
+import ru.Piskovski.HW2.Exercise6;
+import ru.Piskovski.HW2.Exercise7;
+import ru.Piskovski.HW3.Exercise8;
+import ru.Piskovski.HW4.Exercise9;
+import ru.Piskovski.HW4.Exercise10;
+import ru.Piskovski.HW4.Exercise11;
+import ru.Piskovski.HW5.Exercise12;
+
 
 
 public class Menu {
@@ -12,18 +24,18 @@ public class Menu {
             System.out.println("2. Задача 2.");
             System.out.println("3. Задача 3.");
             System.out.println(" Домашнее задание 2");
-            System.out.println("4. Задача 1. ");
-            System.out.println("5. Задача 2. ");
-            System.out.println("6. Задача 3. ");
-            System.out.println("7. Задаа 4. ");
-            System.out.println("--- Урок 3. Коллекции JAVA: Введение ---");
-            System.out.println("9. Задания 1-4. Из списка удалить четные числа, найти min, max, avg значения");
-            System.out.println(" Домашнее задание 3");
-            System.out.println("10. Задание 1. Перевернуть LinkedList");
-            System.out.println("11. Задание 2. Реализовать очередь с помощью LinkedList");
-            System.out.println("12. Задание 3. Добавить отмену последней операции в калькуляторе");
-            System.out.println("--- Урок 5. Хранение и обработка данных ч2: множество коллекций Map ---");
-            System.out.println("13. Задание 1. Написать простой класс Телефонный Справочник");
+            System.out.println("4. Задача 1.");
+            System.out.println("5. Задача 2.");
+            System.out.println("6. Задача 3.");
+            System.out.println("7. Задаа 4.");
+            System.out.println("Домашнее задание 3");
+            System.out.println("8. Задача 1. ");
+            System.out.println(" Домашнее задание 4");
+            System.out.println("9. Задача 1.");
+            System.out.println("10. Задача 2.");
+            System.out.println("11. Задача 3.");
+            System.out.println(" Домашнее задание 5");
+            System.out.println("12. Задача 1.");
             System.out.println("---");
             System.out.println("14. Завершение работы приложения");
             int num = Utilities.inputNaturelNumber("Введите номер задания: ");
@@ -33,40 +45,41 @@ public class Menu {
                     Exercise1.task1();
                     break;
                 case 2:
-//                    Program2.task2();
-//                    break;
-//                case 3:
-//                    Program3.task3();
-//                    break;
-//                case 4:
-//                    Program4.task4();
-//                    break;
-//                case 5:
-//                    Program21.task1();
-//                    break;
-//                case 6:
-//                    Program22.task2();
-//                    break;
-//                case 7:
-//                    Program23.task3();
-//                    break;
-//                case 8:
-//                    Program24.task4();
-//                    break;
-//                case 9:
-//                    Program31.task1234();
-//                    break;
-//                case 10:
-//                    Program41.task1();
-//                    break;
-//                case 11:
-//                    Program42.task2();
-//                    break;
-//                case 12:
-//                    Program43.task3();
-//                    break;
+                    Exercise2.task2();
+                    break;
+                case 3:
+                    Exercise3.task3();
+                    break;
+                case 4:
+                    Exercise4.task1();
+                    break;
+                case 5:
+                    Exercise5.task2();
+                    break;
+                case 6:
+                    Exercise6.task3();
+                    break;
+                case 7:
+                    Exercise7.task4();
+                    break;
+                case 8:
+                    Exercise8.task1234();
+                    break;
+                case 9:
+                    Exercise9.task1();
+                    break;
+                case 10:
+                    Exercise10.task2();
+                    break;
+                case 11:
+                    Exercise11.task3();
+                    break;
+                case 12:
+                    Exercise12.task1();
+
+                    break;
 //                case 13:
-//                    Program51.task1();
+//                    Exercise13.task1();
 //                    break;
                 case 14:
                     flag = false;
